7851aadc542112756deb59e6aa89e144c0f1b5d5
Guildart <49291796+Guildart@users.noreply.github.com>
2019-04-08 15:07:52 +0200

    Update coord.cpp

--- coord.cpp
+++ coord.cpp
@@ -8,8 +8,9 @@ struct coord{
 };
 
 struct EnsCoord {
-  //?
-}
+  coord t[8];
+  int taille;
+};
 
 // On recopie les fonctions du td
 coord creerCoord(int x, int y){
@@ -37,8 +38,25 @@ bool egalCoord(coord a, coord b){
     return true;
 }
 
+void creeEC(EnsCoord &EC){
+  EC.taille = 0; 
+}
 
 coord randomEC()
+  
+EnsCoord trouverVoisins(coord a){
+  EnsCoord EC = creeEC(EC);
+  int x = getX(a);
+  int y = getY(a);
+  for(int i = -1; i <= 1; i++)
+      for(int n = -1; n <= 1; n++)
+        if(x+i != 0 || y+n != 0){
+          EC.t[EC.taille] = {x+i, y+n};
+          EC.taille++;
+        }
+  return EC;
+}
+
 int main() {
   return 0;
 }
