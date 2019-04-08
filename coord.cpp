15ea5496f1e3ad38ca073ce4ed8045e5201acbb3
Paul <paul.mdf@icloud.com>
2019-04-08 16:18:26 +0200

    Quelques fonction en plus que j'ai oublié de commit

--- coord.cpp
+++ coord.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 
+
 struct coord{
   int x;
   int y;
@@ -37,8 +38,46 @@ bool egalCoord(coord a, coord b){
     return true;
 }
 
+int max (int a, int b){
+  if (a>b){
+    return a;
+  }
+  else{
+     return b;
+   }
+}
+
+int min(int a, int b){
+  if (a>b){
+    return b;
+  }
+  else{
+     return a;
+   }
+}
+
+creeEC();// J'ai pas bien compris comment fonctionne EC
+//Genre qu'est ce qu'on met dedans
 
 coord randomEC()
 int main() {
   return 0;
+}
+
+EnsCoord trouverVoisin(coord c){
+  int xmin, xmax, ymin, ymax;
+  EnsCoord e;
+  creeEC(e);
+  xmin = max(getX(c)-1,0);
+  xmax = min(getX(c)+1,size-1);
+  // On doit mettre size comme var global avec la taille du tableau
+  ymin = max(getY(c)-1,0);
+  ymax = min(getY(c)+1,size-1);
+  for (int i = 0; i < xmax; i++) {
+    for (int i = 0; i < ymax; i++) {
+      if(i!=getX(c) || j!=getY(c)){
+        ajouteEC(e,creerCoord(i,j));
+      }
+    }
+  }
 }
