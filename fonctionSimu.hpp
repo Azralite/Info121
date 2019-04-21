b220be37a747b602b60aadc5b124c2a4ff184a67
Guildart <49291796+Guildart@users.noreply.github.com>
2019-04-22 00:43:41 +0200

    Update fonctionSimu.hpp

--- fonctionSimu.hpp
+++ fonctionSimu.hpp
@@ -2,10 +2,22 @@
 
 const int gridSize = 20
 
+int hasard(int val);
+  
 bool seReproduitAnimal(animal a, EnsCoord c);
 
 void initialiseGrille(const grille &g);
 
 void verifieGrille(const grille &g);
 
-EnsCoord voisinsVides(const coord &c, const grille &g);
+EnsCoord voisinsEspece(const grille &g, coord c, espece e);
+
+ensCoord toutEspece(grille g, espece e);
+
+void deplaceTousLapins(grille g1, grille g2);
+
+void deplaceTousRenards(grille g1, grille g2);
+
+void updateGrille(grille g1, grille g2);
+
+void afficheGrille(grille g);
