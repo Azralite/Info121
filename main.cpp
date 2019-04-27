1dba5177d78819aa8a9e0fc30e1108ab0728f9a7
Guildart <romainmussard974@gmail.com>
2019-04-27 20:33:19 +0200

    Réinitilisation du main

--- main.cpp
+++ main.cpp
@@ -6,28 +6,19 @@ void afficheCoord(coord a){
   int y = getY(a);
   std::cout << x << " " << y << '\n';
 }
-/*
-declarer 2 lignes
-utiliser grille vide ou initialise grille (1)
-boucle while
-  afficher grille
-  deplacer tous lapins de l'ancienne grille a la nouvelle
-idem renards
-ancienne <- nouvelle
-nouvelle <- vide
-*/
+
 int main(){
   srand (time(NULL));
   grille g, sauv;
   initialiseGrille(g);
-  //while(nbLapin(g) > 0 && nbRenard(g) > 0){
+  while(nbLapin(g) > 0 && nbRenard(g) > 0){
     grilleVide(sauv);
     updateGrille(g, sauv);
     afficheGrille(sauv,0,0);
     copieGrille(g, sauv);
     afficheGrille(g,nbLapin(g),nbRenard(g));
-//  }
-  grille test;
+  }
+  /*grille test;
   initialiseGrille(test);
   afficheGrille(test,1000,1000);
   EnsCoord a = creeEC();
@@ -38,5 +29,5 @@ int main(){
   for(int i = 0; i < lon; i++){
     b = coordEC(a,i);
 
-  }
+  }*/
 }
