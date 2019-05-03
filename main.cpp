5f269dcd721618881b37bd9fdabb7fb0202e92f8
Guildart <49291796+Guildart@users.noreply.github.com>
2019-05-03 21:15:18 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #define cyan   MLV_rgba(0,175,255,255)
 #define transp   MLV_rgba(0,0,0,0)
 #define rouge   MLV_rgba(200,0,0,255)
+using namespace mlv;
 //int gridSize = 20;
 
 MLV_Image *imageSimulation(grille t){
-  MLV_Image *image = MLV_create_image (10*gridSize, 10*gridSize);
-  for(int i = 0; i < gridSize; i++)
-    for(int n = 0; n < gridSize; n++)
+  MLV_Image *image = MLV_create_image (10*20, 10*20);
+  for(int i = 0; i < 20; i++)
+    for(int n = 0; n < 20; n++)
       if(especeAnimal(t[i][n]) == lapin)
         MLV_draw_circle_on_image(5+i*10, 5+n*10, 5, cyan, image);
       else if(especeAnimal(t[i][n]) == renard)
@@ -26,13 +27,14 @@ int main(){
   srand (time(NULL));
   grille g, sauv;
   initialiseGrille(g);
-
   mlv::window_t foxWar = mlv::window_t( "FoxWar", "essai", 200, 200 );
   while(nbLapin(g) > 0 && nbRenard(g) > 0){
-    MLV_draw_image(imageSimulation(g), 0, 0);
+    MLV_Image *image = imageSimulation(g);
+    MLV_draw_image(image, 0, 0);
+    foxWar.update();
     grilleVide(sauv);
     updateGrille(g, sauv);
-    //afficheGrille(sauv,0,0);
+    afficheGrille(sauv,0,0);
     copieGrille(g, sauv);
     afficheGrille(g,nbLapin(g),nbRenard(g));
     sleep(1);
